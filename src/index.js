c88029bf5fde4f112c5ceb7b47982df1cede9e7e
ankitbhusal <ankitbhusal95@gmail.com>
2023-05-08 18:43:28 +0545

    minor fixes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-// entry point for the application
+import express from 'express';
+import 'dotenv/config.js';
+import dbConfig from './config/dbConfig.js';
+import routes from './routes/index.js';
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.use('/auth', routes.authRoutes);
+
+app.listen(process.env.PORT, () => {
+  dbConfig.authenticate();
+  // dbConfig.sync({ force: true });
+  console.log(`Server is running on port ${ process.env.PORT }`);
+});
