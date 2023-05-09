aa87e39eb9736adeb5cccd27e7f00014d983a388
ankitbhusal <ankitbhusal95@gmail.com>
2023-05-09 08:15:29 +0545

    user login

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,5 +5,6 @@ const router = Router();
 const authController = new AuthController();
 router.post('/register', authController.addUser);
 router.get('/verify/:token', authController.verifyUser);
+router.post('/login', authController.loginUser);
 
 export default router;
