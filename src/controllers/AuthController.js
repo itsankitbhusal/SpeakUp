c5d37c042a1e477ff93d4a8db8191abab81641c1
ankitbhusal <ankitbhusal95@gmail.com>
2023-05-12 16:27:25 +0545

    code refactoring

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -4,23 +4,23 @@ import jwt from 'jsonwebtoken';
 import models from '../models/index.js';
 import message from '../utils/message.js';
 
-class AuthController{
+class AuthController {
   // create user and send verification email
   addUser = async (req, res) => {
     try {
       const { handle, email } = req.body;
-      let { password } = req.body;
-      if (!handle || !password || !email) {
+      let { password: passwordHash } = req.body;
+      if (!handle || !passwordHash || !email) {
         return res.send(message.error('Please provide a handle, email and password'));
       }
       if (email) {
         // check if email is already verified in emails table
-        const forEmail = await models.emails.findOne({ where: { email } });
-        if (forEmail && forEmail.is_verified) {
+        const foundEmail = await models.emails.findOne({ where: { email } });
+        if (foundEmail && foundEmail.is_verified) {
           return res.send(message.error('You cannot register with this email'));
         }
       }
-      password = await bcrypt.hash(password, 10);
+      passwordHash = await bcrypt.hash(passwordHash, 10);
       // generate jwt refresh and access token
       const refreshToken = jwt.sign({ handle }, process.env.JWT_SECRET_REFRESH, {
         expiresIn: '7d'
@@ -29,25 +29,25 @@ class AuthController{
         expiresIn: '15min'
       });
 
-      const user = await models.users.create({ handle, password });
+      const createdUser = await models.users.create({ handle, password: passwordHash });
 
       // token for email verification only
-      const token = jwt.sign({ handle, email }, process.env.JWT_SECRET, {
+      const tokenForEmailVerification = jwt.sign({ handle, email }, process.env.JWT_SECRET, {
         expiresIn: '1d'
       });
 
       // send verification email
-      this.sendMail(email, token);
-      delete user.dataValues.password;
+      this.sendMail(email, tokenForEmailVerification);
+      delete createdUser.dataValues.password;
 
-      if (user) {
+      if (createdUser) {
         return res.send(message.success({
-          user,
+          user: createdUser,
           accessToken,
           refreshToken
         }));
       }
-      
+
     } catch (error) {
       return res.send(message.error(error.message));
     }
@@ -73,7 +73,7 @@ class AuthController{
       subject: 'Verify your email',
       html: `<p>Click <a href="http://localhost:${ port }/auth/verify/${ token }">here</a> to verify your email</p>`
     };
-  
+
     try {
       const info = await transporter.sendMail(mailOptions);
       console.log(`Email sent: ${ info.response }`);
@@ -92,6 +92,12 @@ class AuthController{
     if (!token) {
       return res.send(message.error('Token not provided'));
     }
+    // update user in users table if email is verified
+    const updateUser = async handle => {
+      // verify user in users table
+      await models.users.update({ is_verified: true }, { where: { handle } });
+      return res.send(message.success('User verified'));
+    };
     try {
       // check token validity and verify user
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
@@ -99,18 +105,21 @@ class AuthController{
         return res.send(message.error('Invalid token'));
       }
       const { handle, email } = decoded;
-      const user = await models.users.findOne({ where: { handle } });
-      const forEmail = await models.emails.findOne({ where: { email } });
-      if (forEmail && forEmail.is_verified) {
+      const foundEmail = await models.emails.findOne({ where: { email } });
+
+      if (!foundEmail) {
+        await models.emails.create({ email, is_verified: true });
+        await updateUser(handle);
+        return res.send(message.success('User verified'));
+      }
+      else if (foundEmail && !foundEmail.is_verified) {
+        await models.emails.update({ is_verified: true }, { where: { email } });
+        await updateUser(handle);
+        return res.send(message.success('User verified'));
+      }
+      else {
         return res.send(message.error('You cannot verify with this email'));
       }
-      if (!forEmail) {
-        await models.emails.create({ email, is_verified: true });
-      }
-      // verify user
-      user.is_verified = true;
-      await user.save();
-      return res.send(message.success('User verified'));
     }
     catch (error) {
       return res.send(message.error(error.message));
@@ -123,12 +132,12 @@ class AuthController{
       return res.send(message.error('Please provide a handle and password'));
     }
     try {
-      const user = await models.users.findOne({ where: { handle } });
-      if (!user) {
+      const foundUser = await models.users.findOne({ where: { handle } });
+      if (!foundUser) {
         return res.send(message.error('User not found'));
       }
-      const isMatch = await bcrypt.compare(password, user.password);
-      if (!isMatch) {
+      const isPasswordMatched = await bcrypt.compare(password, foundUser.password);
+      if (!isPasswordMatched) {
         return res.send(message.error('Incorrect password'));
       }
       // generate jwt refresh and access token
@@ -138,9 +147,9 @@ class AuthController{
       const accessToken = jwt.sign({ handle }, process.env.JWT_SECRET_ACCESS, {
         expiresIn: '15min'
       });
-      delete user.dataValues.password;
+      delete foundUser.dataValues.password;
       return res.send(message.success({
-        user,
+        user: foundUser,
         accessToken,
         refreshToken
       }));
