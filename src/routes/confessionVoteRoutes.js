376727c52ed3d6116799dbf86e17c16c3fb93698
ankitbhusal <ankitbhusal95@gmail.com>
2023-05-18 15:04:39 +0545

    fixed function name

--- src/routes/confessionVoteRoutes.js
+++ src/routes/confessionVoteRoutes.js
@@ -1,15 +1,15 @@
 import { Router } from 'express';
-import ConfessionVotesController from '../controllers/ConfessionVotesController.js';
+import ConfessionVoteController from '../controllers/ConfessionVotesController.js';
 import middlewares  from '../middlewares/index.js';
 
 const router = Router();
-const confessionVotesController = new ConfessionVotesController();
+const confessionVoteController = new ConfessionVoteController();
 
-router.post('/up', middlewares.verifyAccessToken, confessionVotesController.createConfessionUpVote);
-router.post('/down', middlewares.verifyAccessToken, confessionVotesController.createConfessionDownVote);
-router.put('/up', middlewares.verifyAccessToken, confessionVotesController.updateConfessionUpVote);
-router.put('/down', middlewares.verifyAccessToken, confessionVotesController.updateConfessionDownVote);
-router.delete('/up', middlewares.verifyAccessToken, confessionVotesController.deleteConfessionUpVote);
-router.delete('/down', middlewares.verifyAccessToken, confessionVotesController.deleteConfessionDownVote);
+router.post('/up', middlewares.verifyAccessToken, confessionVoteController.createConfessionUpVote);
+router.post('/down', middlewares.verifyAccessToken, confessionVoteController.createConfessionDownVote);
+router.put('/up', middlewares.verifyAccessToken, confessionVoteController.updateConfessionUpVote);
+router.put('/down', middlewares.verifyAccessToken, confessionVoteController.updateConfessionDownVote);
+router.delete('/up', middlewares.verifyAccessToken, confessionVoteController.deleteConfessionUpVote);
+router.delete('/down', middlewares.verifyAccessToken, confessionVoteController.deleteConfessionDownVote);
 
 export default router;
