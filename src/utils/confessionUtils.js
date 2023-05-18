c8a5ab3aa44b53416eabfc9a2ae123c9120bdb9e
ankitbhusal <ankitbhusal95@gmail.com>
2023-05-18 14:45:57 +0545

    sanitize input function enhanced

--- src/utils/confessionUtils.js
+++ src/utils/confessionUtils.js
@@ -1,4 +1,3 @@
-import validator from 'validator';
 import { JSDOM } from 'jsdom';
 import DOMPurify from 'dompurify';
 
@@ -6,24 +5,18 @@ const { window } = new JSDOM('');
 const purify = DOMPurify(window);
 
 const sanitizeInput = async input => {
-
   input = await input.toString();
-
+  // remove html tags
+  input = await purify.sanitize(input);
   // remove leading and trailing spaces
   input = await input.trim();
-    
   // remove extra spaces between words
   input = await input.replace(/\s+/g, ' ');
-  console.log('\n\n\n\nbefore purify');
-  console.log(input);
   input = purify.sanitize(input);
-    
   // limit to 1000 characters
   if (input.length > 1000) {
     input = await input.substring(0, 1000);
   }
-  console.log('\n\n\n\nafter purify');
-  console.log(input);
   return input;
 };
 
