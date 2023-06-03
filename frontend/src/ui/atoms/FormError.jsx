9e873ce1c2df3226189ed345f0ae26abc698161d
Ankit Bhusal <53370039+itsankitbhusal@users.noreply.github.com>
2023-06-03 19:16:14 +0545

    Delete FormError.jsx

--- frontend/src/ui/atoms/FormError.jsx
+++
@@ -1,7 +0,0 @@
-const FormError = ({ error, className }) => (
-  <div className={` absolute  text-danger text-sm italic w-full flex justify-end items-center ${ className }`}>
-    {error}
-  </div>
-);
-    
-export default FormError;
