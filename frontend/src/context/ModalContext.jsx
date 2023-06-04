63fd4a57ddfd3eb5cd6be8d167f7087ca69d1005
ankitbhusal <ankitbhusal95@gmail.com>
2023-06-04 22:31:45 +0545

    modal context

---
+++ frontend/src/context/ModalContext.jsx
@@ -0,0 +1,22 @@
+import { createContext, useState } from 'react';
+
+const ModalContext = createContext();
+
+
+const ModalProvider = ({ children }) => { 
+  const [showModal, setShowModal] = useState(false);
+
+  const OpenModal = () => {
+    setShowModal(true);
+  };
+  const CloseModal = () => {
+    setShowModal(false);
+  };
+  return (
+    <ModalContext.Provider value={{ showModal, OpenModal, CloseModal }}>
+      {children}
+    </ModalContext.Provider>
+  );
+};
+
+export { ModalContext, ModalProvider };
