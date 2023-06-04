161ddf2ac71d851cab1b039fc7018a5b3a7e655a
ankitbhusal <ankitbhusal95@gmail.com>
2023-06-04 22:31:00 +0545

    create confession modal

--- frontend/src/ui/atoms/CreateConfession.jsx
+++ frontend/src/ui/atoms/CreateConfession.jsx
@@ -1,7 +1,7 @@
-const CreateConfession = ({ onClick }) => (
+const CreateConfession = () => (
   <>
-    <div onClick={onClick} className='bg-cwhite text-cblack outline-dashed outline-primary rounded-sm px-16 py-4 outline-[1.5px] w-full hover:cursor-pointer my-4'>
-            What's your confession today?
+    <div  className='bg-cwhite text-cblack outline-dashed outline-primary rounded-sm px-16 py-4 outline-[1.5px] w-full hover:cursor-pointer my-4'>
+        What's your confession today?
     </div>
   </>
 );
--- frontend/src/ui/atoms/Dialog.jsx
+++ frontend/src/ui/atoms/Dialog.jsx
@@ -1,11 +1,9 @@
-const Modal = ({ children, className }) => (
-  <>
-    <div className="fixed z-50 inset-0 flex items-center justify-center bg-black bg-opacity-50 shadow-2xl">
-      <div className={`modal bg-white rounded-sm shadow-lg ${ className }`}>
-        <div>{children}</div>
-      </div>
+const Dialog = ({ children, className }) => (
+  <div className="fixed z-50 inset-0 flex items-center justify-center bg-black bg-opacity-50 shadow-2xl">
+    <div className={`modal bg-white rounded-sm shadow-lg ${ className }`}>
+      <div>{children}</div>
     </div>
-  </>
+  </div>
 );
 
-export default Modal;
+export default Dialog;
