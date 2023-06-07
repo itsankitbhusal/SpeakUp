651f5c426859c9d047befe56ca899d032b98b648
ankitbhusal <ankitbhusal95@gmail.com>
2023-06-07 13:37:43 +0545

    confession context

---
+++ frontend/src/context/ConfessionContext.jsx
@@ -0,0 +1,38 @@
+import { createContext, useEffect, useState } from 'react';
+import { getApprovedConfessions } from '../services/confessions';
+
+const ConfessionContext = createContext();
+
+const ConfessionProvider = ({ children }) => {
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(0);
+    
+  const [confessions, setConfessions] = useState([]);
+  const getConfession = async () => {
+    const response = await getApprovedConfessions(limit, page);
+    if (response.success) {
+      const data = await response.data;
+      setConfessions(data.confessions);
+    } else {
+      console.log( response.message);
+    }
+  };
+  const handleLimitChange = event => {
+    setLimit(event.target.value);
+  };
+  const handlePageChange = event => {
+    setPage(event.target.value);
+  };
+    
+  // on change of page or limit, get the confessions
+  useEffect(() => {
+    getConfession();
+  }, [page, limit]);
+    
+  return (
+    <ConfessionContext.Provider value={{ confessions, handleLimitChange, handlePageChange }}>
+      {children}
+    </ConfessionContext.Provider>
+  );};
+
+export { ConfessionContext, ConfessionProvider };
