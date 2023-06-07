e0067df121ee635d390671d18085fa008e3e859d
ankitbhusal <ankitbhusal95@gmail.com>
2023-06-07 21:59:51 +0545

    parse body with react-markdown

--- frontend/src/ui/molecules/ConfessionPost.jsx
+++ frontend/src/ui/molecules/ConfessionPost.jsx
@@ -4,23 +4,26 @@ import Line from '../atoms/Line';
 import Text from '../atoms/Text';
 import VotesBtn from './VotesBtn';
 import { FaCommentAlt } from 'react-icons/fa';
+import ReactMarkdown from 'react-markdown';
 
-const ConfessionPost = ({ handle, date, views,title, body }) => (
-  <div >
+const ConfessionPost = ({ handle, date, views, title, body }) => (
+  <div>
     <UserDetail handle={handle} date={date} views={views} />
-    <Heading heading="h4" className="text-black text-base text-justify">{title}</Heading>
-
-    <Line />
-    <div className=' flex justify-center items-start gap-2'>
+    <Heading heading="h4" className="text-black text-base text-justify">
+      {title}
+    </Heading>
+    <Line className="mt-0" />
+    <div className=" flex justify-center items-start gap-2">
       <div className="w-1/12 flex flex-col justify-between items-center gap-6">
         <VotesBtn view="1.5k" />
-        <FaCommentAlt className=' text-primary text-xl' />
+        <FaCommentAlt className=" text-primary text-xl" />
       </div>
-      <div className=' w-11/12'>
-        <Text className=" text-justify text-[.9rem]" >{ body}</Text>
+      <div className=" w-11/12">
+        <Text div className=" text-justify text-[.9rem]">
+          <ReactMarkdown className=" grid gap-2">{body}</ReactMarkdown>
+        </Text>
       </div>
     </div>
   </div>
 );
-
 export default ConfessionPost;
