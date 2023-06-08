4209c599277025d7966a4247b6cea772e30b9010
ankitbhusal <ankitbhusal95@gmail.com>
2023-06-08 14:01:26 +0545

    minor fixes

--- frontend/src/ui/organisms/Confession.jsx
+++ frontend/src/ui/organisms/Confession.jsx
@@ -2,11 +2,17 @@ import Line from '../atoms/Line';
 import ConfessionPost from '../molecules/ConfessionPost';
 import WriteComment from '../molecules/WriteComment';
 
-const Confession = ({ handle, date, views, title, body }) => (
-  
-  <div className=' my-8'>
-    <ConfessionPost handle={handle} date={date} views={views} title={title} body={body} />
-    <div className=' my-4'>
+const Confession = ({ handle, date, views, title, body, confessionId }) => (
+  <div className=" my-8">
+    <ConfessionPost
+      confessionId={confessionId}
+      handle={handle}
+      date={date}
+      views={views}
+      title={title}
+      body={body}
+    />
+    <div className=" my-4">
       <WriteComment />
     </div>
     <Line />
