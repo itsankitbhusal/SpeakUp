ff4beb4bfa59b3c25ea4a5878940cac755ad3361
ankitbhusal <ankitbhusal95@gmail.com>
2023-06-09 15:47:59 +0545

    added routes required for views

--- src/routes/viewRoutes.js
+++ src/routes/viewRoutes.js
@@ -9,6 +9,7 @@ router.post('/:confessionId', middlewares.verifyAccessToken, middlewares.verifyR
 router.get('/:id', middlewares.verifyAccessToken, middlewares.verifyRefreshToken, viewController.getViewsByConfessionId);
 router.get('/count/:id', middlewares.verifyAccessToken, middlewares.verifyRefreshToken, viewController.getTotalViewsByConfessionId);
 router.get('/user/:id', middlewares.verifyAccessToken, middlewares.verifyRefreshToken, viewController.getViewsByUserId);
+router.get('/user/confession/:cid', middlewares.verifyAccessToken, middlewares.verifyRefreshToken, viewController.getConfessionViewsByUserId);
 router.delete('/:id', middlewares.verifyAccessToken, middlewares.verifyRefreshToken, viewController.deleteView);
 
 export default router;
