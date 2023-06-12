35092ecdb420398df8b5ce86c6be7fe32551e954
ankitbhusal <ankitbhusal95@gmail.com>
2023-06-12 19:19:42 +0545

    added hasMore and loading state

--- frontend/src/context/ConfessionContext.jsx
+++ frontend/src/context/ConfessionContext.jsx
@@ -4,33 +4,34 @@ import { getApprovedConfessions } from '../services/confessions';
 const ConfessionContext = createContext();
 
 const ConfessionProvider = ({ children }) => {
-  const [limit, setLimit] = useState(10);
+  const [limit, setLimit] = useState(2);
   const [page, setPage] = useState(0);
-    
+  const [isLoading, setIsLoading] = useState(false);
   const [confessions, setConfessions] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
+
   const getConfession = async () => {
+    if (!hasMore) {return;}
+    setIsLoading(true);
     const response = await getApprovedConfessions(limit, page);
     if (response.success) {
-      const data = await response.data;
-      setConfessions(data.confessions);
+      const { data } = response;
+      if (data.confessions.length < limit) {
+        setHasMore(false);
+      }
+      setConfessions(prevConfessions => [...prevConfessions, ...data.confessions]);
     } else {
       throw new Error(response.message);
     }
+    setIsLoading(false);
   };
-  const handleLimitChange = event => {
-    setLimit(event.target.value);
-  };
-  const handlePageChange = event => {
-    setPage(event.target.value);
-  };
-    
   // on change of page or limit, get the confessions
   useEffect(() => {
     getConfession();
   }, [page, limit]);
     
   return (
-    <ConfessionContext.Provider value={{ confessions, handleLimitChange, handlePageChange }}>
+    <ConfessionContext.Provider value={{ confessions, setPage, isLoading, hasMore }}>
       {children}
     </ConfessionContext.Provider>
   );};
