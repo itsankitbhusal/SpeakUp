faf7986eab2c21ad2596bccc1f61ddf82150891a
ankitbhusal <ankitbhusal95@gmail.com>
2023-06-13 22:43:54 +0545

    added get user by id | for profile

--- frontend/src/services/auth.jsx
+++ frontend/src/services/auth.jsx
@@ -1,8 +1,9 @@
 import api from './api.jsx';
+const route = '/auth';
 
 export const login = async userData => {
   try {
-    const response = await api.post('/auth/login', {
+    const response = await api.post(`${ route }/login`, {
       ...userData
     });
     return await response.data;
@@ -13,9 +14,18 @@ export const login = async userData => {
 
 export const register = async userData => {
   try {
-    const response = await api.post('/auth/register', {
+    const response = await api.post(`${ route }/register`, {
       ...userData
     });
+    return await response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const getUserById = async() => {
+  try {
+    const response = await api.get(`${ route }/me`);
     return await response.data;
   } catch (error) {
     throw new Error(error);
