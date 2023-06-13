6fd64f1406352cfd791514fd47d46a813a919236
ankitbhusal <ankitbhusal95@gmail.com>
2023-06-13 22:41:35 +0545

    inifinte scroll confession for user

---
+++ frontend/src/ui/organisms/ProfileConfessions.jsx
@@ -0,0 +1,63 @@
+import { useContext, useEffect, useRef } from 'react';
+import Text from '../atoms/Text';
+import Confession from '../organisms/Confession';
+import { dateConverter } from '../../utils/dateConverter';
+import Loading from '../atoms/Loading';
+import { ProfileContext } from '../../context/ProfileContext';
+
+const ProfileConfessions = ({ className }) => {
+  const bottom = useRef(null);
+  const confessionRef = useRef(null);
+  //   const { confessions, setPage, isLoading, hasMore } = useContext(ConfessionContext);
+  const { confessions, setPage, isLoading, hasMore } = useContext(ProfileContext);
+  
+  
+  // handle observer
+  const handleObserver = entries => {
+    const target = entries[0];
+    if (target.isIntersecting && hasMore) {
+      setPage(prevPage => prevPage + 1);
+    }
+  };
+  
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: '20px',
+      threshold: 1 // Change threshold to detect partial visibility
+    };
+  
+    const observer = new IntersectionObserver(handleObserver, options);
+    if (bottom.current) {
+      observer.observe(bottom.current);
+    }
+  }, [ bottom ]);
+
+  return (
+    <div className={`grid place-items-center mt-32 max-w-[40vw] ${ className }`}>
+      <div ref={confessionRef}>
+        {confessions?.map(confession => (
+          <Confession
+            key={confession.id}
+            confessionId={confession.id}
+            handle={confession.user.handle}
+            date={dateConverter(confession.created_at)}
+            views={confession.views_count}
+            title={confession.title}
+            body={confession.body}
+          />
+        ))}
+        {isLoading && (
+          <div className="flex justify-center w-full">
+            <Loading />
+          </div>
+        )}
+        <div ref={bottom} className='my-8' >
+          <Text className="text-center text-gray-400">{ !isLoading && 'End of page'}</Text>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileConfessions;
