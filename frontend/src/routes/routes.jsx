785e2ba2b125e52be2b53a3570029988e49d3d86
ankitbhusal <ankitbhusal95@gmail.com>
2023-06-13 22:38:43 +0545

    profile route added

--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -1,6 +1,7 @@
 import Home from '../ui/pages/Home';
 import Login from '../ui/pages/Login';
 import NotFound from '../ui/pages/NotFound';
+import Profile from '../ui/pages/Profile';
 import Register from '../ui/pages/Register';
 
 const routes = [{
@@ -12,6 +13,9 @@ const routes = [{
 }, {
   path: '/register',
   element: <Register />
+}, {
+  path: '/profile',
+  element: <Profile />
 }, {
   path: '*',
   element: <NotFound />
