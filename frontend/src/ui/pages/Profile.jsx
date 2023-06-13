0cf18265ee171bdf0c57dd3329cebaaa8a625fc4
ankitbhusal <ankitbhusal95@gmail.com>
2023-06-13 22:43:19 +0545

    profile provider

--- frontend/src/ui/pages/Profile.jsx
+++ frontend/src/ui/pages/Profile.jsx
@@ -1,20 +1,16 @@
-import { useState } from 'react';
 import Navbar from '../organisms/Navbar';
 import ProfileTemplate from '../templates/ProfileTemplate';
 import { ProfileProvider } from '../../context/ProfileContext';
 
-const Profile = () => {
-  const [user, setUser] = useState(null);
-  return (
-    <>
-      <Navbar />
-      <div className='mx-[20vw] my-10'>
-        <ProfileProvider>
-          <ProfileTemplate user={user} />
-        </ProfileProvider>
-      </div>
-    </>
-  );
-};
+const Profile = () => (
+  <>
+    <Navbar />
+    <div className='mx-[20vw] my-10'>
+      <ProfileProvider>
+        <ProfileTemplate />
+      </ProfileProvider>
+    </div>
+  </>
+);
 
 export default Profile;
