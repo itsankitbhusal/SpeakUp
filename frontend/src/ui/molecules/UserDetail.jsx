7d5acfd10f3129cebfd27f3d237bdec32252d7bb
ankitbhusal <ankitbhusal95@gmail.com>
2023-06-14 17:38:58 +0545

    added reporting feature

--- frontend/src/ui/molecules/UserDetail.jsx
+++ frontend/src/ui/molecules/UserDetail.jsx
@@ -1,21 +1,65 @@
 import { useEffect, useState } from 'react';
+import decode from 'jwt-decode';
 import { useNavigate } from 'react-router-dom';
 import { AiFillEye, AiOutlineEdit, AiOutlineDelete } from 'react-icons/ai';
+import { MdOutlineReportProblem } from 'react-icons/md';
 import { FcApproval } from 'react-icons/fc';
 import { BsThreeDotsVertical } from 'react-icons/bs';
 import Text from '../atoms/Text';
 import Button from '../atoms/Button';
 import { deleteConfession } from '../../services/confessions';
+import { createReport } from '../../services/report';
 import { showToast } from '../../utils/toast';
 
 const UserDetail = ({ handle, date, views, isApproved, isProfile, confessionId }) => {
   const navigate = useNavigate();
 
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isReportMenuOpen, setIsReportMenuOpen] = useState(false);
+  const [isUser, setIsUser] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [reportMessage, setReportMessage] = useState('');
+  
+
+  useEffect(() => {
+    const token = localStorage.getItem('access');
+    if (token) {
+      const decodedToken = decode(token);
+      if (decodedToken.handle === handle) {
+        setIsUser(true);
+      }
+    }
+  }, []);
 
   const handleDotsClick = () => {
     setIsMenuOpen(!isMenuOpen);
   };
+
+  const handleBtnClick = () => {
+    setIsReportMenuOpen(!isReportMenuOpen);
+  };
+
+  const handleReport = async() => {
+    console.log('report: ', confessionId);
+    setIsModalOpen(true);
+    if (!reportMessage) {
+      return;
+    }
+    const reportData = {
+      'reportType': 'confession',
+      confessionId,
+      'description': reportMessage
+    };
+
+    const response = await createReport(reportData);
+    if (response.success) {
+      showToast('Report submitted successfully', 'success');
+      setIsModalOpen(false);
+    } else {
+      showToast(response.message, 'error');
+    }
+  };
+  
   const handleDelete = async() => {
     const confirm = window.confirm('Are you sure you want to delete this confession?');
     if (confirm) {
@@ -35,14 +79,15 @@ const UserDetail = ({ handle, date, views, isApproved, isProfile, confessionId }
   };
 
   useEffect(() => {
-    // after 3 seconds, close the menu
     const timeout = setTimeout(() => {
       setIsMenuOpen(false);
+      setIsReportMenuOpen(false);
     }
     , 3000);
     return () => clearTimeout(timeout);
 
-  }, [isMenuOpen]);
+  }, [isMenuOpen, isReportMenuOpen]);
+
   return(
     <div className='flex justify-between items-center w-full'>
       <div className=' flex justify-between items-center gap-1 text-md'>
@@ -59,8 +104,12 @@ const UserDetail = ({ handle, date, views, isApproved, isProfile, confessionId }
         )}
         {isApproved && (<FcApproval />)}
         <div className=' relative'>
-          {isProfile && (
+          {isProfile ? (
             <div onClick={handleDotsClick} className='hover:cursor-pointer hover:text-primaryDark'>
+              < BsThreeDotsVertical className=' text-primary' />
+            </div>
+          ) : !isUser && (
+            <div onClick={handleBtnClick} className='hover:cursor-pointer hover:text-primaryDark'>
               < BsThreeDotsVertical className=' text-primary' />
             </div>
           )}
@@ -68,9 +117,28 @@ const UserDetail = ({ handle, date, views, isApproved, isProfile, confessionId }
             <div id='menu' className='absolute -top-2 left-8 bg-white rounded-sm shadow-md p-2'>
               <Button variant='icon' onClick={handleEdit} className=' text-base font-normal'><AiOutlineEdit />Edit</Button>
               <Button variant='icon' onClick={handleDelete} className=' text-base font-normal'><AiOutlineDelete />Delete</Button>
-              {/* <Button variant='icon' onClick={handleReport} className=' text-base font-normal'><MdOutlineReportProblem />Report</Button> */}
             </div>
           )}
+          {isReportMenuOpen &&  (
+            <div id='menu' className='absolute -top-2 left-8 bg-white rounded-sm shadow-md p-2'>
+              <Button variant='icon' onClick={handleReport} className=' text-base font-normal'><MdOutlineReportProblem />Report</Button>
+            </div>
+          )}
+          {
+            isModalOpen && (
+              <div className='fixed z-50 top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center'>
+                <div className='w-5/12 bg-white rounded-sm shadow-md p-4'>
+                  <Text className='text-lg font-semibold'>Report Confession</Text>
+                  <Text className='text-sm'>Please enter a message to report this confession</Text>
+                  <textarea onChange={e => setReportMessage(e.target.value)} className='w-full h-32 border border-gray-300 rounded-sm p-2 mt-2' placeholder='Enter message here...'></textarea>
+                  <div className='flex justify-end gap-2 mt-2'>
+                    <Button variant='secondary' onClick={() => setIsModalOpen(false)}>Cancel</Button>
+                    <Button variant='primary' onClick={handleReport}>Submit</Button>
+                  </div>
+                </div>
+              </div>
+            )
+          }
         </div>
       </div>
     </div>
