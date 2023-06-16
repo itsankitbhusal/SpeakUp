48ee35df8d4bebc9182772104cc2cb79d545f5b6
ankitbhusal <ankitbhusal95@gmail.com>
2023-06-16 21:51:44 +0545

    dashboard with Outlet

--- frontend/src/Dashboard/Dashboard.jsx
+++ frontend/src/Dashboard/Dashboard.jsx
@@ -1,10 +1,19 @@
+import { Outlet } from 'react-router-dom';
+import Links from './compoenents/Links';
 import { DashboardProvider } from '../context/DashboardContext';
-import DashboardPage from '../Dashboard/Pages/DashboardPage';
 
-const Dashboard = () => (
+const Dashboard = ({ children }) => (
   <>
     <DashboardProvider>
-      <DashboardPage />
+      <div className=' flex mx-[8vw] max-h-screen gap-8 justify-between'>
+        <div className='w-4/12'>
+          <Links />
+        </div>
+        <div className='w-8/12 grid place-items-center'>
+          <Outlet />
+        </div>
+      </div>
+      {children}
     </DashboardProvider>
   </>
 );
