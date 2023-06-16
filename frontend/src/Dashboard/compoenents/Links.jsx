74ab299b420ede830727e59be782e1fe19b03add
ankitbhusal <ankitbhusal95@gmail.com>
2023-06-16 21:53:15 +0545

    sidebar links of dashboard

--- frontend/src/Dashboard/compoenents/Links.jsx
+++ frontend/src/Dashboard/compoenents/Links.jsx
@@ -19,12 +19,15 @@ const Links = () => {
     navigate('/login');
   };
   return(
-    <div className=' w-full h-screen'>
+    <div className=' w-full h-screen shadow-xl'>
       <div className=' max-w-[24vw] h-screen p-4'>
-        <h1 className=' text-3xl font-black text-center'>Dashboard</h1>
+        <h1 className=' text-3xl font-bold text-center mt-16 leading-[3rem]'>
+          <span className='font-black text-primary grid place-items-center text-4xl tracking-tighter'>SpeakUp</span>
+          <span className='text-cblack'>Dashboard</span>
+        </h1>
         <Line />
         <div className=' grid place-items-center h-4/6'>
-          <div >
+          <div className=' -mt-48' >
             <div className='w-full grid place-items-center'>
               <Link className='w-full' to='/dashboard'>
                 <Button variant="ghost" className="w-full bg-transparent hover:bg-cwhite font-semibold">
@@ -40,7 +43,7 @@ const Links = () => {
                   </div>
                 </Button>
               </Link>
-              <Link className='w-full' to='/dashboard/confession'>
+              <Link className='w-full' to='/dashboard/confessions'>
                 <Button variant="ghost" className="w-full bg-transparent hover:bg-cwhite font-semibold">
                   <div className=' flex items-center gap-4 justify-start w-full'>
                     <BsFilesAlt />Confessions
