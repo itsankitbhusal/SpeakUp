1548f176fa624c5751cd2ae8bc494dc28a50847c
ankitbhusal <ankitbhusal95@gmail.com>
2023-06-18 23:36:09 +0545

    routes for analytics

---
+++ src/routes/analyticsRoutes.js
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import AnalyticsController from '../controllers/AnalyticsController.js';
+import middlewares from '../middlewares/index.js';
+
+const router = Router();
+const analyticsController = new AnalyticsController();
+
+router.get('/growth-rate', middlewares.verifyAccessToken, middlewares.verifyRefreshToken, analyticsController.getGrowthRate);
+router.get('/roles', middlewares.verifyAccessToken, middlewares.verifyRefreshToken, analyticsController.userRoleDistribution);
+router.get('/verification', middlewares.verifyAccessToken, middlewares.verifyRefreshToken, analyticsController.verificationStatus);
+router.get('/confession-approval', middlewares.verifyAccessToken, middlewares.verifyRefreshToken, analyticsController.confessionApprovalStatus);
+router.get('/up-down', middlewares.verifyAccessToken, middlewares.verifyRefreshToken, analyticsController.upDownVoteRatio);
+
+export default router;
