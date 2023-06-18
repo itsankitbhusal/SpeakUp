a55308afbc98af90ff05e2cc275dfd5482471bc3
ankitbhusal <ankitbhusal95@gmail.com>
2023-06-18 23:38:12 +0545

    minor fix

--- frontend/src/Dashboard/Pages/DashHome.jsx
+++ frontend/src/Dashboard/Pages/DashHome.jsx
@@ -2,10 +2,8 @@ import Card from '../compoenents/Card';
 
 const DashHome = () => (
   <>
-    <div className=' grid place-items-center'>
-      <div className=' grid  grid-cols-3 gap-8'>
-        <Card />
-      </div>
+    <div className='w-full h-full grid place-items-center'>
+      <Card />
     </div>
   </>
 );
