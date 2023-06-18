23875aa997b6dd98fe33e4c227bcc408f6d76d85
ankitbhusal <ankitbhusal95@gmail.com>
2023-06-18 23:37:55 +0545

    different charts

--- frontend/src/Dashboard/compoenents/Card.jsx
+++ frontend/src/Dashboard/compoenents/Card.jsx
@@ -1,13 +1,36 @@
-import Heading from '../../ui/atoms/Heading';
+import { useState ,useEffect } from 'react';
 
-const Card = () => (
-  <div className='rounded-sm py-8 px-16 outline outline-1 outline-cwhite hover:shadow-lg hover:cursor-pointer transition-all'>
-    <Heading heading='h1'>User</Heading>
-    <div>
-      <div>245</div>
-      <div>Posts</div>
+import { getGrowthRate } from '../../services/analytics';
+import AreaChart from './AreaChart';
+import PieChart from './PieChart';
+import DoughnutChart from './DougnutChart';
+
+const Card = () => {
+  const [data, setData] = useState({
+    date: null,
+    growthRate: null
+  });
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await getGrowthRate();
+      setData(prev => ({
+        ...prev,
+        date: response.data.date,
+        growthRate: response.data.growthRate
+      }));
+    };
+    getData();
+  }, []);
+  return(
+    <div className='rounded-sm max-h-screen py-8 px-16 outline outline-1 outline-cwhite hover:shadow-sm hover:cursor-pointer transition-all overflow-y-scroll scrollbar-hidden'>
+      <div className='grid place-content-center gap-24 my-16 '>
+        <AreaChart data={data} />
+        <PieChart />
+        <DoughnutChart />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Card;
