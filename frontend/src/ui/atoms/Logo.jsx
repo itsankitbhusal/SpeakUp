82aa3f4859d856b84bd704735267c78650aa99b7
ankitbhusal <ankitbhusal95@gmail.com>
2023-07-09 23:20:08 +0545

    updated width and height from css

--- frontend/src/ui/atoms/Logo.jsx
+++ frontend/src/ui/atoms/Logo.jsx
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.svg';
 import logoWide from '../../assets/logo-wide.svg';
 
-const Logo = ({ wide, className, width, height }) => (
+const Logo = ({ wide, className }) => (
   <Link to="/">
-    <img width={width} height={height} draggable="false" className={`hover:cursor-pointer${ className }`}  src={wide ? logoWide: logo} alt={wide? 'SpeakUp wide logo': 'SpeakUp logo'} />
+    <img draggable="false"  className={`w-full h-full hover:cursor-pointer${ className }`}  src={wide ? logoWide: logo} alt={wide? 'SpeakUp wide logo': 'SpeakUp logo'} />
   </Link>
 );
 
