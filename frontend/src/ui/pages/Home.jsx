bb30094629461dcefe6734567fce60f2ddf714a1
ankitbhusal <ankitbhusal95@gmail.com>
2023-07-09 23:24:25 +0545

    bg color

--- frontend/src/ui/pages/Home.jsx
+++ frontend/src/ui/pages/Home.jsx
@@ -18,11 +18,11 @@ const Home = () => {
 
   return(
     <>
-      <div className=' flex justify-center items-center'>
+      <div className=' flex justify-center items-center bg-cwhite'>
         <NavFull />
-        <div className=' grid place-items-center'>
+        <div className=' grid place-items-center w-8.5/12'>
           <div className=' flex w-full justify-evenly gap-0 items-start'>
-            <div className='sticky top-20 w-1/4 mt-20 pt-2  flex justify-start'>
+            <div className='sticky top-20 w-1/4 mt-20 pt-2'>
               <SidebarFull />
             </div>
             <div className=' w-3/4 flex justify-end' >
