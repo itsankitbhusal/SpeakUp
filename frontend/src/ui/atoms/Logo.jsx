0e873e0b16df47fa3c44c380732402c544126ce4
ankitbhusal <ankitbhusal95@gmail.com>
2023-07-10 12:24:40 +0545

    responsive logo

--- frontend/src/ui/atoms/Logo.jsx
+++ frontend/src/ui/atoms/Logo.jsx
@@ -2,10 +2,11 @@ import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.svg';
 import logoWide from '../../assets/logo-wide.svg';
 
-const Logo = ({ wide, className }) => (
-  <Link to="/">
-    <img draggable="false"  className={`w-full h-full hover:cursor-pointer${ className }`}  src={wide ? logoWide: logo} alt={wide? 'SpeakUp wide logo': 'SpeakUp logo'} />
-  </Link>
+const Logo = () => (
+  <div className=' flex justify-center items-center'>
+    <img draggable="false"  className={'w-full h-full hover:cursor-pointer md:hidden '}  src={logo} alt={'SpeakUp logo'} />
+    <img draggable="false"  className={'w-full h-full hover:cursor-pointer hidden md:block '}  src={logoWide} alt={'SpeakUp wide logo'} />
+  </div>
 );
 
 
