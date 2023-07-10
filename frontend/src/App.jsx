c54d51922f5ab84efff793756e05264854954ceb
ankitbhusal <ankitbhusal95@gmail.com>
2023-07-10 18:01:32 +0545

    navbar provider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
 import { useRoutes } from 'react-router-dom';
-import { MyContext } from './context/MyContext';
 import routes from './routes/routes';
 import { ToastContainer } from './utils/toast';
+import { NavbarProvider } from './context/NavbarContext';
 
 const App = () => {
   const routing = useRoutes(routes);
   return (
     <>
-      <MyContext.Provider value={{}}>
+      <NavbarProvider>
         <ToastContainer />
         {routing}
-      </MyContext.Provider>
+      </NavbarProvider>
     </>
   );
 };
