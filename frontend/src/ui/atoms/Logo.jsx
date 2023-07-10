53be5d5399f7a48e76b1f4ec11d2334a6a3a27eb
ankitbhusal <ankitbhusal95@gmail.com>
2023-07-10 18:04:36 +0545

    single logo

--- frontend/src/ui/atoms/Logo.jsx
+++ frontend/src/ui/atoms/Logo.jsx
@@ -4,8 +4,7 @@ import logoWide from '../../assets/logo-wide.svg';
 
 const Logo = () => (
   <div className=' flex justify-center items-center'>
-    <img draggable="false"  className={'w-full h-full hover:cursor-pointer md:hidden '}  src={logo} alt={'SpeakUp logo'} />
-    <img draggable="false"  className={'w-full h-full hover:cursor-pointer hidden md:block '}  src={logoWide} alt={'SpeakUp wide logo'} />
+    <img draggable="false"  className={'w-full h-full hover:cursor-pointer'}  src={logoWide} alt={'SpeakUp wide logo'} />
   </div>
 );
 
