c19a284b24c04871facd5928431e1a087ef68062
ankitbhusal <ankitbhusal95@gmail.com>
2023-07-11 19:18:17 +0545

    responsive dashboard

--- frontend/src/Dashboard/Dashboard.jsx
+++ frontend/src/Dashboard/Dashboard.jsx
@@ -5,11 +5,11 @@ import { DashboardProvider } from '../context/DashboardContext';
 const Dashboard = ({ children }) => (
   <>
     <DashboardProvider>
-      <div className=' flex mx-[8vw] max-h-screen gap-8 justify-between'>
-        <div className='w-4/12'>
+      <div className=' block lg:flex mx-4 max-h-screen gap-8 justify-between transition-all'>
+        <div className='w-full lg:w-4/12'>
           <Links />
         </div>
-        <div className='w-8/12 grid place-items-center'>
+        <div className='w-full lg:w-8/12 grid place-items-center'>
           <Outlet />
         </div>
       </div>
