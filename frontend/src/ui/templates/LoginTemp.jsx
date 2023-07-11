08590f154a596180e64f2ec8b48957183b02cf04
ankitbhusal <ankitbhusal95@gmail.com>
2023-07-11 16:46:12 +0545

    responsive login page

--- frontend/src/ui/templates/LoginTemp.jsx
+++ frontend/src/ui/templates/LoginTemp.jsx
@@ -3,8 +3,8 @@ import FormHeader from '../molecules/FormHeader';
 
 const LoginTemp = () => (
   
-  <div className=' grid place-items-center min-h-screen'>
-    <div className=' grid place-items-center gap-10 px-16 py-8 rounded-sm shadow-2xl'>
+  <div className='grid place-items-center min-h-screen '>
+    <div className='grid place-items-center gap-10 px-2 sm:px-8 lg:px-16 py-2 sm:py-4 lg:py-8 rounded-sm sm:shadow-lg md:shadow-xl shadow-none lg:shadow-2xl'>
       <FormHeader  className="">User Login </FormHeader>
       <LoginFields />
     </div>
