b934ea6b1e28b8241c175f2c0187f98f69561143
ankitbhusal <ankitbhusal95@gmail.com>
2023-07-13 21:59:26 +0545

    loading state for user data

--- frontend/src/ui/molecules/ProfileHeader.jsx
+++ frontend/src/ui/molecules/ProfileHeader.jsx
@@ -11,7 +11,7 @@ import Line from '../atoms/Line';
 import Button from '../atoms/Button';
 
 const ProfileHeader = () => {
-  const { user, getUserData, profileAvatar } = useContext(ProfileContext);
+  const { user, getUserData, userDataLoading, profileAvatar } = useContext(ProfileContext);
 
   const dateFormat = dateString => {
     const date = new Date(dateString);
@@ -26,51 +26,74 @@ const ProfileHeader = () => {
   return (
     <div className='w-full flex flex-col items-center justify-center white'>
       <div className='w-full flex flex-col items-center justify-center mt-4'>
-        <div className='absolute w-full -top-0 sm:top-24 left-0'>
+        <div className='absolute -top-0 sm:top-24 left-0 inline-block'>
           <Link to="/">
             <Button variant="ghost" ><MdArrowBackIos />Back</Button>
           </Link>
         </div>
-        <div className='w-full inline-grid place-items-center text-primary font-black'>
-          <div className=' inline-grid place-items-center'>
-            <div className=' flex items-center flex-col sm:flex-row gap-4 justify-center'>
-              <img src={user && profileAvatar} alt='av' className='w-16 h-16 rounded-full bg-primaryLight' />
-              <span className=' text-xl sm:text-2xl md:text-3xl lg:text-4xl'>
-                {user ? `@${ user.handle }` : 'handle not found'}
-              </span>
-            </div>
-            <div className='w-full -mt-2'>
-              <div className='flex flex-col items-end justify-center'>
-                <div className='text-base font-normal text-cblack'><span>Joined on </span><span className=' font-semibold'>{ user? dateFormat(user.created_at): '0' } </span></div>
+        {userDataLoading && (
+          <div className='w-full inline-grid place-items-center text-primary font-black'>
+            <div className=' inline-grid place-items-center'>
+              <div className=' flex items-center flex-col sm:flex-row gap-4 justify-center'>
+                <img src={profileAvatar} alt='av' className='w-16 h-16 rounded-full bg-primaryLight' />
+                <span className=' text-xl sm:text-2xl md:text-3xl lg:text-4xl'>
+                  Loading...
+                </span>
+              </div>
+              <div className='w-full -mt-2'>
+                <div className='flex flex-col items-end justify-center'>
+                  <div className='text-base font-normal text-cblack'><span>Joined on </span><span className=' font-semibold'>0</span></div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <Line />
-        <div className='flex w-full items-center justify-center mt-4 gap-2 md:gap-8 flex-wrap'>
-          <div className='flex flex-col items-center justify-center'>
-            <div className='text-sm sm:text-base font-bold'>Confessions</div>
-            <div className='text-base md:text-lg lg:text-xl font-bold flex items-center gap-2 justify-center text-cblack'><BsPostcard className='-mt-1' />{ user? user.confessionCount: '0'}</div>
-          </div>
-          <div className='flex flex-col items-center justify-center'>
-            <div className='text-sm sm:text-base font-bold'>Comments</div>
-            <div className='text-base md:text-lg lg:text-xl font-bold flex items-center gap-2 justify-center text-cblack'><FaRegComments className='-mt-1' />{ user? user.commentCount: '0'}</div>
-          </div>
-          <div className='flex flex-col items-center justify-center'>
-            <div className='text-sm sm:text-base font-bold'>Total views</div>
-            <div className='text-base md:text-lg lg:text-xl font-bold flex items-center gap-2 justify-center text-cblack'><AiOutlineEye className='-mt-1' />{user? user.totalViews: '0'}</div>
-          </div>
-          <div className='flex flex-col items-center justify-center'>
-            <div className='text-sm sm:text-base font-bold'>Top Confession</div>
-            <div className='text-base md:text-lg lg:text-xl font-bold flex items-center gap-2 justify-center text-cblack'><AiOutlineTrophy className='-mt-1' />{ user?.highestViewedConfession?.view_count? user?.highestViewedConfession?.view_count :'0'}</div>
-          </div>
-          <div className='flex flex-col items-center justify-center'>
-            <div className='text-sm sm:text-base font-bold'>Top Vote</div>
-            <div className='text-base md:text-lg lg:text-xl font-bold flex items-center gap-2 justify-center text-cblack'><BiUpvote className='-mt-1' />{ user?.highestUpvotedConfession?.upvote_count ? user?.highestUpvotedConfession?.upvote_count : 0}</div>
-          </div>
-        </div>
+        )}
+       
+        {!userDataLoading && (
+          <>
+  
+            <div className='w-full inline-grid place-items-center text-primary font-black'>
+              <div className=' inline-grid place-items-center'>
+                <div className=' flex items-center flex-col sm:flex-row gap-4 justify-center'>
+                  <img src={user && profileAvatar} alt='av' className='w-16 h-16 rounded-full bg-primaryLight' />
+                  <span className=' text-xl sm:text-2xl md:text-3xl lg:text-4xl'>
+                    {user ? `@${ user.handle }` : 'handle not found'}
+                  </span>
+                </div>
+                <div className='w-full -mt-2'>
+                  <div className='flex flex-col items-end justify-center'>
+                    <div className='text-base font-normal text-cblack'><span>Joined on </span><span className=' font-semibold'>{ user? dateFormat(user.created_at): '0' } </span></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <Line />
+            <div className='flex w-full items-center justify-center mt-4 gap-2 md:gap-8 flex-wrap'>
+              <div className='flex flex-col items-center justify-center'>
+                <div className='text-sm sm:text-base font-bold'>Confessions</div>
+                <div className='text-base md:text-lg lg:text-xl font-bold flex items-center gap-2 justify-center text-cblack'><BsPostcard className='-mt-1' />{ user? user.confessionCount: '0'}</div>
+              </div>
+              <div className='flex flex-col items-center justify-center'>
+                <div className='text-sm sm:text-base font-bold'>Comments</div>
+                <div className='text-base md:text-lg lg:text-xl font-bold flex items-center gap-2 justify-center text-cblack'><FaRegComments className='-mt-1' />{ user? user.commentCount: '0'}</div>
+              </div>
+              <div className='flex flex-col items-center justify-center'>
+                <div className='text-sm sm:text-base font-bold'>Total views</div>
+                <div className='text-base md:text-lg lg:text-xl font-bold flex items-center gap-2 justify-center text-cblack'><AiOutlineEye className='-mt-1' />{user? user.totalViews: '0'}</div>
+              </div>
+              <div className='flex flex-col items-center justify-center'>
+                <div className='text-sm sm:text-base font-bold'>Top Confession</div>
+                <div className='text-base md:text-lg lg:text-xl font-bold flex items-center gap-2 justify-center text-cblack'><AiOutlineTrophy className='-mt-1' />{ user?.highestViewedConfession?.view_count? user?.highestViewedConfession?.view_count :'0'}</div>
+              </div>
+              <div className='flex flex-col items-center justify-center'>
+                <div className='text-sm sm:text-base font-bold'>Top Vote</div>
+                <div className='text-base md:text-lg lg:text-xl font-bold flex items-center gap-2 justify-center text-cblack'><BiUpvote className='-mt-1' />{ user?.highestUpvotedConfession?.upvote_count ? user?.highestUpvotedConfession?.upvote_count : 0}</div>
+              </div>
+            </div>
+            <Line className='mt-2 mb-0 max-w-[40vw]' />
+          </>
+        )}
       </div>
-      <Line className='mt-2 mb-0 max-w-[40vw]' />
     </div>
   );
 };
