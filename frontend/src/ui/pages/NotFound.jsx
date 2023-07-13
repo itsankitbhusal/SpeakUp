07511b78a55c05532c8746c65ddbeac6c16633cb
ankitbhusal <ankitbhusal95@gmail.com>
2023-07-13 22:30:27 +0545

    responsive 404 page

--- frontend/src/ui/pages/NotFound.jsx
+++ frontend/src/ui/pages/NotFound.jsx
@@ -1,4 +1,5 @@
 import Button from '../atoms/Button';
+import Heading from '../atoms/Heading';
 import Text from '../atoms/Text';
 import { Link } from 'react-router-dom';
 
@@ -6,14 +7,14 @@ const NotFound = () => (
   <>
     <div className=' grid place-items-center min-h-screen'>
       <div className='grid place-items-center gap-2 p-8 rounded-sm'>
-        <Text className=" text-7xl text-bold text-[12rem] text-dangerLight -mb-12 ">404</Text>
-        <Text className=" text-base">Ooops! Page Not Found</Text>
+        <Heading heading="h1" className="text-3xl -mb-4 sm:text-6xl sm:-mb-8 md:text-7xl font-normal md:-mb-16">404</Heading>
+        <Text className=" text-sm sm:text-base">Ooops! Page Not Found</Text>
         <div className=' grid place-items-center'>
-          <Text className="text-md">This page doesn't exist or was removed!</Text>
-          <Text className="text-md">We suggest you back to home</Text>
+          <Text className="text-sm sm:text-base">This page doesn't exist or was removed!</Text>
+          <Text className="text-sm sm:text-base">We suggest you back to home</Text>
         </div>
         <Link to="/">
-          <Button className="mt-4" variant="outline-primary" >Back to home</Button>
+          <Button className="mt-0 text-sm sm:text-base sm:mt-2 md:mt-4 font-normal" variant="outline-primary" >Back to home</Button>
         </Link>
       </div>
     </div>
