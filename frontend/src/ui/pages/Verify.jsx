1a436cfae43bffb903c0c74ac228a0e6d87ed374
ankitbhusal <ankitbhusal95@gmail.com>
2023-07-17 14:33:59 +0545

    verify user page

--- frontend/src/ui/pages/Verify.jsx
+++ frontend/src/ui/pages/Verify.jsx
@@ -1,13 +1,44 @@
-import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { verifyUser } from '../../services/emails';
+import { showToast } from '../../utils/toast';
 
 const Verify = () => {
   const { token } = useParams();
-  return (
-    <>
-      <div>verify</div>
-      {token}
-    </>
-  );
+  const [tokenValue, setTokenValue] = useState('');
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (token === undefined || token === null || token === '') {
+      setTokenValue('');
+    } else {
+      setTokenValue(token);
+    }
+  }, [token]);
+
+  useEffect(() => {
+    const verifyUserWithAccount = async () => {
+      try {
+        const response = await verifyUser(tokenValue);
+        if (response.success) {
+          showToast('User verification success', 'success');
+          navigate('/login');
+        } else {
+          showToast('User verification failed', 'error');
+          navigate('/login');
+        }
+        
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    if (tokenValue !== '') {
+      verifyUserWithAccount();
+    }
+  }, [tokenValue]);
+
+  return null;
 };
 
 export default Verify;
