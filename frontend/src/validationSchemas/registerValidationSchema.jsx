71c6a84d4e2492794297973195d488fdbde7ef8f
itsankitbhusal <ankitbhusal95@gmail.com>
2023-09-28 16:03:32 +0545

    validation of email and alphanumeric handle

--- frontend/src/validationSchemas/registerValidationSchema.jsx
+++ frontend/src/validationSchemas/registerValidationSchema.jsx
@@ -1,9 +1,20 @@
 import * as Yup from 'yup';
 
 export const registerValidationSchema = Yup.object({
-  handle: Yup.string().required('Required'),
-  email: Yup.string().email('Invalid email address').required('Required'),
-  password: Yup.string().required('Required').min(8, 'Must be 8 characters or more').max(30, 'Must be 30 characters or less'),
+  handle: Yup.string()
+    .matches(/^[a-zA-Z]+([a-zA-Z0-9]{4,30})$/, 'Invalid username')
+    .required('Required'),
+  email: Yup.string()
+    .email('Invalid email address')
+    .matches(
+      /^[a-zA-Z][_a-zA-Z0-9-]+(\.[_a-zA-Z0-9-]+)*@[a-zA-Z][_a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*(\.[a-zA-Z]{2,})$/,
+      'Invalid email address'
+    )
+    .required('Required'),
+  password: Yup.string()
+    .required('Required')
+    .min(8, 'Must be 8 characters or more')
+    .max(30, 'Must be 30 characters or less'),
   cpassword: Yup.string()
     .oneOf([Yup.ref('password'), null], 'Passwords must match')
     .required('Required'),
